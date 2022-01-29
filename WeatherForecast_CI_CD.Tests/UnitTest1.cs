89d5e39de14ab7c125038280aa7cf60cec788e10
heyder <elesgerovheyder@gmail.com>
2022-01-29 19:46:05 +0400

    Add tests

---
+++ WeatherForecast_CI_CD.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace WeatherForecast_CI_CD.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
